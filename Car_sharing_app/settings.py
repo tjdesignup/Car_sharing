90e7cac1d16b265a861dd91f49bbcd47e2f1c090
Tomáš Jordán <200249@vutbr.cz>
2025-06-23 15:57:52 +0200

    Update requirements and settigns

--- Car_sharing_app/settings.py
+++ Car_sharing_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_-5#(28non)5*yu=z))*3gl)2gijasv2lm4&i&r!+u&dv&k@7^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["car-sharing.onrender.com"]
 
 
 # Application definition
@@ -74,10 +75,11 @@ WSGI_APPLICATION = 'Car_sharing_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
